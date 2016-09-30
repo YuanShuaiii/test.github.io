61b7a2d361dbc9b629452b27d7ff7dde8acbbbf2
YuanShuai <yuanshuai0729@gmail.com>
2016-09-30 16:40:42 +0800

    new

---
+++ javascript/mainScroll.js
@@ -0,0 +1,39 @@
+var main = document.getElementsByClassName("main");
+function scroll(){
+    if (window.pageYOffset){
+        return {
+            top: window.pageYOffset,
+            left: window.pageXOffset
+        };
+    } else if (document.body.scrollTop){
+        return {
+            top: document.body.scrollTop,
+            left: document.body.scrollLeft
+        };
+    } else {
+        return {
+            top: document.documentElement.scrollTop,
+            left: document.documentElement.scrollLeft
+        };
+    }
+}
+window.onscroll = function() {
+    console.log(main[0].offsetTop);
+    var Y = scroll().top;
+    if(Y > document.body.clientHeight / 4){
+        // current = scroll().top;
+        clearInterval(window.timer);
+        window.timer = setInterval(function () {
+            main[0].style.opacity = Y / 900;
+            var speed = (document.body.clientHeight - Y) / 50 ;
+            speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+            Y += speed;
+            window.scrollTo(0, Y);
+            if (document.body.clientHeight == Y) {
+                clearInterval(window.timer);
+            }
+        }, 15);
+        window.onscroll = null
+    }
+};
+// console.log();
--- javascript/mainpage.js
+++
@@ -1,4 +0,0 @@
-var n1 = document.getElementsByClassName("n1");
-n1[0].onclick = function () {
-    n1[0].style.backgroundColor = "red"
-};
